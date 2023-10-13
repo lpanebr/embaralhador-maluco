8ec31b901eb0255db16c67bc0b4f1f28a89f54c3
Luciano Panepucci <lpanebr@gmail.com>
2023-10-13 11:38:04 -0300

    embaralha preservando mais caracteres para palavras longas.

--- embaralhar.py
+++ embaralhar.py
@@ -1,21 +1,40 @@
 import random
+import sys
+
+def get_next_int_by_range(word, start_value, ranges):
+    sorted_ranges = sorted(ranges) 
+    current_value = start_value
+    for cur_max in sorted_ranges:
+        if len(word) <=cur_max:
+            return current_value
+        current_value += 1
+    return current_value
+
+def get_word_parts(word, start_size, end_size):
+    if start_size < 0 or end_size < 0 or start_size + end_size > len(word):
+        return "Erro: Tamanhos inválidos."
+    inicio = word[:start_size]
+    meio = word[start_size:-end_size] if start_size + end_size < len(word) else ""
+    fim = word[-end_size:] if end_size > 0 else ""
+    return [inicio, meio, fim]
 
 def embaralhar_palavras(texto):
     palavras = texto.split()
     texto_embaralhado = []
 
     for palavra in palavras:
-        if len(palavra) > 2:
-            letras_intermediarias = list(palavra[1:-1])
-            random.shuffle(letras_intermediarias)
-            palavra_embaralhada = palavra[0] + ''.join(letras_intermediarias) + palavra[-1]
+        start_size = end_size = get_next_int_by_range(palavra, 0, [3,5,8,12])
+        if len(palavra) >= start_size + end_size:
+            partes = get_word_parts(palavra, start_size, end_size)
+            meio = list(partes[1])
+            random.shuffle(meio)
+            palavra_embaralhada = partes[0] + ''.join(meio) + partes[2]
             texto_embaralhado.append(palavra_embaralhada)
         else:
             texto_embaralhado.append(palavra)
 
     return ' '.join(texto_embaralhado)
 
-texto_original = input("Digite o texto que você deseja embaralhar: ")
+texto_original = sys.argv[1]
 texto_embaralhado = embaralhar_palavras(texto_original)
-print("Texto embaralhado: ", texto_embaralhado)
-
+print(texto_embaralhado)
